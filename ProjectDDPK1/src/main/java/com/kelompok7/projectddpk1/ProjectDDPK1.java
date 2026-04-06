c50667cbec530824ee5d6711f7f2ca1417b7731d
faridl312 <a.ridl27.10@gmail.com>
2026-04-06 11:45:02 +0800

    Farid nambahin file

---
+++ ProjectDDPK1/src/main/java/com/kelompok7/projectddpk1/ProjectDDPK1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kelompok7.projectddpk1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ProjectDDPK1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
